3c7d7f46631f4ed394e6c3c2dc98d63c6704d202
Gabriel Ng <119044507+Gabareal@users.noreply.github.com>
2026-01-15 21:53:34 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let cursor = document.querySelector('.customCursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+});
