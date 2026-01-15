86c512b723be55030069b54d757300027c61656e
Gabriel Ng <119044507+Gabareal@users.noreply.github.com>
2026-01-15 21:53:07 +0800

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-let cursor = document.querySelector('.customCursor');
-
-document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
-});
