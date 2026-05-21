a5d28ea3c77e536052ae80515a6bc30f182f1617
Gabareal <gabrielngyes@gmail.com>
2026-05-21 12:55:52 +0800

    hello

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let cursor = document.querySelector('.customCursor');
-
+//hello
 document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;");
 });
